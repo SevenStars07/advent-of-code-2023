2c563bb9dce412dbeafb9a87dab72adb2327944d
Dragos Adrian <justsevenstars07@gmail.com>
2023-12-05 13:32:18 +0200

    day 4✅

---
+++ day-4/part-1/Program.cs
@@ -0,0 +1,29 @@
+﻿// var fileName = "example.txt";
+var fileName = "input.txt";
+
+var lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", fileName));
+
+var totalPoints = 0d;
+
+foreach (var line in lines)
+{
+    var splitNumbers = line.Split(":")[1].Split("|");
+
+    var winningNumbers = splitNumbers[0].Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x.Trim()));
+    var myNumbers = splitNumbers[1].Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x.Trim()));
+
+    var matchedNumbers = myNumbers.Count(x => winningNumbers.Contains(x));
+
+    totalPoints += GetPoints(matchedNumbers);
+}
+
+Console.WriteLine(totalPoints);
+
+double GetPoints(int matchedNumbers)
+{
+    if (matchedNumbers == 0)
+    {
+        return 0;
+    }
+    return Math.Pow(2, matchedNumbers - 1);
+}
---
+++ day-4/part-2/Program.cs
@@ -0,0 +1,25 @@
+﻿// var fileName = "example.txt";
+var fileName = "input.txt";
+
+var lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", fileName));
+
+var totalNumberOfCards = Enumerable.Repeat(1, lines.Length).ToList();
+
+for (var i = 0; i < lines.Length; i++)
+{
+    var line = lines[i];
+
+    var splitNumbers = line.Split(":")[1].Split("|");
+
+    var winningNumbers = splitNumbers[0].Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x.Trim()));
+    var myNumbers = splitNumbers[1].Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x.Trim()));
+
+    var matchedNumbers = myNumbers.Count(x => winningNumbers.Contains(x));
+
+    for (var j = 1; j <= matchedNumbers; j++)
+    {
+        totalNumberOfCards[i + j] += totalNumberOfCards[i];
+    }
+}
+
+Console.WriteLine(totalNumberOfCards.Sum());
