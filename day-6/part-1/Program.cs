c74cde24928e816b1220dd866579e690cbff6cde
Dragos Adrian <justsevenstars07@gmail.com>
2023-12-06 11:37:35 +0200

    day 6✅

---
+++ day-6/part-1/Program.cs
@@ -0,0 +1,40 @@
+﻿// var fileName = "example.txt";
+var fileName = "input.txt";
+
+var lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", fileName));
+
+var times = lines[0].Split(":")[1].Trim().Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x));
+var distances =  lines[1].Split(":")[1].Trim().Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x));
+
+var races = times.Zip(distances, (a, b) => (int.Parse(a.Trim()), int.Parse(b.Trim())));
+
+var totalWaysToWin = 1;
+
+foreach (var race in races)
+{
+    var numberOfWaysToWinRace = GetNumberOfSecondsToHoldForWin(race);
+
+    totalWaysToWin *= numberOfWaysToWinRace;
+}
+
+Console.WriteLine(totalWaysToWin);
+
+int GetNumberOfSecondsToHoldForWin((int, int) race)
+{
+    var wins = new List<int>();
+
+    var time = race.Item1;
+    var raceDistanceToWin = race.Item2;
+
+    for (var i = 1; i < time; i++)
+    {
+        var speed = i;
+
+        var distance = (time - i) * speed;
+
+        if (distance > raceDistanceToWin)
+            wins.Add(speed);
+    }
+
+    return wins.Count;
+}
---
+++ day-6/part-2/Program.cs
@@ -0,0 +1,28 @@
+﻿// var fileName = "example.txt";
+var fileName = "input.txt";
+
+var lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", fileName));
+
+var time = lines[0].Split(":")[1].Trim().Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x)).Aggregate((a,b)=>a+b);
+var distance =  lines[1].Split(":")[1].Trim().Split(" ").Where(x=>!string.IsNullOrWhiteSpace(x)).Aggregate((a,b)=>a+b);
+
+var numberOfWaysToWinRace = GetNumberOfSecondsToHoldForWin((double.Parse(time), double.Parse(distance)));
+
+Console.WriteLine(numberOfWaysToWinRace);
+
+double GetNumberOfSecondsToHoldForWin((double, double) race)
+{
+    var numberOfWins = 0;
+
+    var raceDistanceToWin = race.Item2;
+
+    for (var i = 1; i < race.Item1; i++)
+    {
+        var distanceThisRace = (race.Item1 - i) * i;
+
+        if (distanceThisRace > raceDistanceToWin)
+            numberOfWins++;
+    }
+
+    return numberOfWins;
+}
