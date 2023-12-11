8bc9ca199e2e0fd457d21f9370776f4668072b19
Dragos Adrian <justsevenstars07@gmail.com>
2023-12-11 17:33:29 +0200

    day 9✅

---
+++ day-9/part-1/Program.cs
@@ -0,0 +1,37 @@
+﻿// var fileName = "example.txt";
+var fileName = "input.txt";
+
+var lines = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", fileName))
+    .ToList();
+
+var total = 0L;
+
+lines.ForEach(line =>
+{
+    var history = line.Split(" ").Select(long.Parse).ToList();
+
+    var sequences = new List<List<long>>();
+
+    var currentSequence = history;
+
+    sequences.Add(currentSequence);
+
+    while (currentSequence.Any(l => l != 0))
+    {
+        var newSequence = new List<long>();
+
+        for (var i = 0; i < currentSequence.Count - 1; i++)
+        {
+            newSequence.Add(currentSequence[i + 1] - currentSequence[i]);
+        }
+
+        currentSequence = newSequence;
+        sequences.Add(currentSequence);
+    }
+
+    var nextData = sequences.Sum(l => l.Last());
+
+    total += nextData;
+});
+
+Console.WriteLine(total);
